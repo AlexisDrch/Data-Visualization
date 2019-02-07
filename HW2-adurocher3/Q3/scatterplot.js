08032284315ef10616bfefcca4cbee93db02758b
Alexis Durocher <aleksi.durocher@wanadoo.fr>
2019-02-06 20:24:33 -0500

    explanation q2

--- HW2-adurocher3/Q3/scatterplot.js
+++ HW2-adurocher3/Q3/scatterplot.js
@@ -1,63 +1,33 @@
 
-movies = {};
-
-d3.csv("movies.csv", d => {
-	// parse string
-	return {
-		Id : +d.Id,
-		Title : d.Title,
-		Year : +d.Year,
-		Runtime : +d.Runtime,
-		Country: d.Country,
-		Rating: +d.Rating,
-		Votes: +d.votes,
-		Budget: +d.Budget,
-		Gross: +d.Gross,
-		WinsNoms: +d.WinsNoms,
-		IsGoodRating: +d.IsGoodRating
-	};
-
-}).then(dataset => {
-	var w = 1000;
-	var h = 300;
-	var padding = 50;
-
-	// a.1
-	//Define linear scalers
-	var xScale_a_1 = d3.scaleLinear()
-		.domain([0, d3.max(dataset, d=> d.Rating)])
-		.range([padding, w - padding * 2]);
-
-	var yScale_a_1 = d3.scaleLinear()
-		.domain([0, d3.max(dataset, d=> d.WinsNoms)])
-		.range([h - padding, padding]);
-
-	//Create SVG element
-	var svg = d3.select("body")
-				.append("svg")
-				.attr("width", w)
-				.attr("height", h);
-
+function scatterPlot(dataset, featureX, featureY, svg, xScale, yScale, title = 'Your scatterplot',
+	xlabel= 'X axis', ylabel= 'Y axis', w=1000, h=500, padding=100, featureSized = false) {
 	var r = 5;
-	var cross = r*2
+	var cross = r*2;
 	
 	//Define X axis
 	var xAxis = d3.axisBottom()
-		.scale(xScale_a_1);
-
+		.scale(xScale);
 	//Define Y axis
 	var yAxis = d3.axisLeft()
-		.scale(yScale_a_1);
+		.scale(yScale);
 
-	//Create circles
-	svg.selectAll("circle.data")
+	var rScale= d3.scaleLinear()
+		.domain([d3.min(dataset, d => d[featureSized]),
+			d3.mean(dataset, d => d[featureSized]),
+			d3.max(dataset, d => d[featureSized])])
+		.range([1, 5, 15]);
+
+	svg.selectAll("circle.circle")
 	   .data(dataset.filter(d=>!(d.IsGoodRating)))
 	   .enter()
 	   .append("circle")
-	   .attr("class", "data bad_rating")
-	   .attr("cx", d => xScale_a_1(d.Rating))
-	   .attr("cy", d => yScale_a_1(d.WinsNoms))
-	   .attr("r",  d => r)
+	   .attr("class", "circle bad_rating")
+	   .attr("cx", d => xScale(d[featureX]))
+	   .attr("cy", d => yScale(d[featureY]))
+	   .attr("r",  d => {
+	   	if (featureSized) {
+	   		return rScale(d[featureSized]); 
+	   } return r})
 
 	//Create h line for cross
 	svg.selectAll("rect.hline")
@@ -65,10 +35,16 @@ d3.csv("movies.csv", d => {
 	   .enter()
 	   .append("rect")
 	   .attr("class", "hline good_rating")
-	   .attr("x", d => xScale_a_1(d.Rating) - cross/2)
-	   .attr("y", d => yScale_a_1(d.WinsNoms) - cross/4)
-	   .attr("width", cross)
-	   .attr("height", cross/2);
+	   .attr("x", d => xScale(d[featureX]) - cross/2)
+	   .attr("y", d => yScale(d[featureY]) - cross/4)
+	   .attr("width", d => {
+	   	if (featureSized) {
+	   		return 2*rScale(d[featureSized]); 
+	   } return cross})
+	   .attr("height", d => {
+	   	if (featureSized) {
+	   		return rScale(d[featureSized]); 
+	   } return cross/2});
 	
 	//Create v line for cross
 	svg.selectAll("rect.vline")
@@ -76,10 +52,16 @@ d3.csv("movies.csv", d => {
 	   .enter()
 	   .append("rect")
 	   .attr("class", "vline good_rating")
-	   .attr("x", d => xScale_a_1(d.Rating) - cross/4)
-	   .attr("y", d => yScale_a_1(d.WinsNoms) - cross/2)
-	   .attr("width", cross/2)
-	   .attr("height", cross);
+	   .attr("x", d => xScale(d[featureX]) - cross/4)
+	   .attr("y", d => yScale(d[featureY]) - cross/2)
+	   .attr("width", d => {
+	   	if (featureSized) {
+	   		return rScale(d[featureSized]); 
+	   } return cross/2})
+	   .attr("height", d => {
+	   	if (featureSized) {
+	   		return 2*rScale(d[featureSized]); 
+	   } return cross});
 
 	//Create X axis
 	svg.append("g")
@@ -95,14 +77,14 @@ d3.csv("movies.csv", d => {
 
 	//Create legend
 	svg.append("text")
-		.text("Wins+Nominations vs. Rating")
+		.text(title)
 		.attr("x", w/2 - padding/2)
 	    .attr("y", padding/2)
 		.attr("font-family", "sans-serif")
 		.attr("font-size", "15px")
 	
 	svg.append("circle")// add circle legend
-		.attr("r",  d => r)
+		.attr("r",  r)
 		.attr("cx", w - padding - r*2)
 		.attr("cy", padding/2 -r)
 		.attr("class", "bad_rating")
@@ -134,29 +116,150 @@ d3.csv("movies.csv", d => {
 		.attr("class", "legend")
 
 	svg.append("text")
-		.text("Wins+Noms")
+		.text(xlabel)
 		.attr("x", w/2 - padding/2)
 	    .attr("y", h - padding/2)
 	    .attr("class", "legend")
 
 	svg.append("text")
-		.text("Rating")
+		.text(ylabel)
 		.attr("transform", "rotate(-90)")
 		.attr("x", -h/2)
 	    .attr("y", padding/4)
 	    .attr("class", "legend")
+}
 
-	/*[6 points] Create two scatter plots, one for each feature combination specified below. 
-		In the scatter plots, visualize “good rating” class instances as blue crosses, 
-		and “bad rating” instances as red circles. Add a legend to the top right corner 
-		showing the symbols’ mapping to the classes.
-		
-		Feature 10 (Wins and nominations) vs. Feature 6 ( Rating)
+
+
+
+
+d3.csv("movies.csv", d => {
+	// parse string
+	return {
+		Id : +d.Id,
+		Title : d.Title,
+		Year : +d.Year,
+		Runtime : +d.Runtime,
+		Country: d.Country,
+		Rating: +d.Rating,
+		Votes: +d.Votes,
+		Budget: +d.Budget,
+		Gross: +d.Gross,
+		WinsNoms: +d.WinsNoms,
+		IsGoodRating: +d.IsGoodRating
+	};
+
+}).then(dataset => {
+	var w = 1000;
+	var h = 700;
+	var padding = 100;
+
+	var r = 5;
+	var cross = r*2
+
+	// a.1
+	/*Feature 10 (Wins and nominations) vs. Feature 6 ( Rating)
 		Figure title: Wins+Nominations vs. Rating
 		X axis (horizontal) label: Rating
 		Y axis (vertical) label: Wins+Noms*/
+	var svg = d3.select("body")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h);
+
+	var xScale_a_1 = d3.scaleLinear()
+		.domain([0, d3.max(dataset, d=> d.Rating)])
+		.range([padding, w - padding * 2]);
+
+	var yScale_a_1 = d3.scaleLinear()
+		.domain([0, d3.max(dataset, d=> d.WinsNoms)])
+		.range([h - padding, padding]);
+
+	scatterPlot(dataset, "Rating", "WinsNoms", svg, xScale_a_1, yScale_a_1,
+		"Wins+Nominations vs. Rating", "Rating", "Wins+Noms", w, h, padding);
+
+	//a.1 bis
+	/*Feature 8 (Budget) vs. Features 6 ( Rating)
+		Figure title: Budget vs. Rating
+		X axis (horizontal) label: Rating
+		Y axis (vertical) label: Budget
+	*/	
+	var svg2 = d3.select("body")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h)
+				.attr("y", h);
+
+	var yScale_a_2 = d3.scaleLinear()
+		.domain([0, d3.max(dataset, d=> d.Budget)])
+		.range([h - padding, padding]);
+
+	scatterPlot(dataset, "Rating", "Budget", svg2, xScale_a_1, yScale_a_2,
+		"Budget vs. Rating", "Rating", "Budget", w, h, padding);
+
+
+	//b.1
+	/*Feature 7 (Votes) vs. Feature 6 (Rating) sized by Feature 10 (Wins+Nominations)
+		Figure title: Votes vs. Rating sized by Wins+Nominations
+		X axis (horizontal) label: Rating
+		Y axis (vertical) label: Votes
+	*/	
+	var svg3 = d3.select("body")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h)
+				.attr("y", 2*h);
+
+	var yScale_b_1 = d3.scaleLinear()
+		.domain([0, d3.max(dataset, d=> d.Votes)])
+		.range([h - padding, padding]);
+
+	scatterPlot(dataset, "Rating", "Votes", svg3, xScale_a_1, yScale_b_1,
+		"Votes vs. Rating sized by Wins+Nominations", "Rating", "Votes", w, h, padding, "WinsNoms");
+
+	//c.1
+	/*First Figure: uses the square root scale for its y-axis (only)
+	Figure title: Wins+Nominations (square-root-scaled) vs. Rating
+	X axis (horizontal) label: Rating
+	Y axis (vertical)  label: Wins+Noms*/
+
+	var svg4 = d3.select("body")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h)
+				.attr("y", 3*h);
+
+	var yScale_c_1 = d3.scalePow().exponent(0.5)
+		.domain([0, d3.max(dataset, d=> d.WinsNoms)])
+		.range([h - padding, padding]);
+
+	scatterPlot(dataset, "Rating", "WinsNoms", svg4, xScale_a_1, yScale_c_1,
+		"Wins+Nominations (square-root-scaled) vs. Rating", "Rating", "Wins+Noms", w, h, padding);
+	
+
+	//c.2
+	/*Second Figure: uses the log scale for its y-axis (only)
+	Figure title: Wins+Nominations (log-scaled) vs. Rating
+	X axis (horizontal) label: Rating
+	Y axis (vertical)  label: Wins+Noms*/
+
+	var svg5 = d3.select("body")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h)
+				.attr("y", 4*h);
+
+	var yScale_c_2 = d3.scaleLog()
+		.domain([5e-1, d3.max(dataset, d=> d.WinsNoms)])
+		.range([h - padding, padding]);
+
+	// remove 0 from dataset
+	dataset_ = dataset.map(d => {d.WinsNoms = (d.WinsNoms == 0)? 5e-1: d.WinsNoms; return d;})
+
+
+	scatterPlot(dataset_, "Rating", "WinsNoms", svg5, xScale_a_1, yScale_c_2,
+		"Wins+Nominations (log-scaled) vs. Rating", "Rating", "Wins+Noms", w, h, padding);
 	
 	console.log(dataset[0]);
-
 
 })
